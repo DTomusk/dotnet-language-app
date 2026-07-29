4e92772b853d76fd00e5ccc4d8841b400e7ff69b
Dave <david.tomusk@gmail.com>
2026-07-29 16:49:26 +0100

    Move language selector to home page

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,10 +1,54 @@
 import { Button, Stack, Typography } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 import { useLanguageStats } from "../features/languagePractice/hooks/useLanguageStats";
+import { useGetActiveLanguage, useSetActiveLanguage } from "../features/languagePractice/hooks/useActiveLanguage";
+import { useAvailableLanguages } from "../features/languagePractice/hooks/useAvailableLanguages";
+import Spinner from "../components/Spinner";
+import Alert from "@mui/material/Alert";
+import LanguageSelector from "../features/languagePractice/components/LanguageSelector";
+import { useState } from "react";
+import { useTranslation } from "react-i18next";
 
 export default function HomePage() {
     const navigate = useNavigate();
-    const { data } = useLanguageStats();
+    const { t } = useTranslation(["common"]);
+    
+    const { data: languageStats } = useLanguageStats();
+
+    const { data, isLoading, error, refetch } = useGetActiveLanguage();
+    const { data: languages, isLoading: isLoadingLanguages } = useAvailableLanguages();
+    const setLanguageMutation = useSetActiveLanguage();
+    const [selectedLanguage, setSelectedLanguage] = useState("");
+
+    const onConfirmLanguage = async () => {
+        if (!selectedLanguage) {
+            return;
+        }
+
+        await setLanguageMutation.mutateAsync({ languageCode: selectedLanguage });
+        await refetch();
+    };
+
+    if (isLoading) {
+        return <Spinner aria-label={t("common:loading")} />;
+    }
+
+    if (error) {
+        return <Alert severity="error">{t("common:error")}: {error.message}</Alert>;
+    }
+
+    if (!data) {
+        return (
+            <LanguageSelector
+                languages={languages}
+                isLoading={isLoadingLanguages}
+                isSubmitting={setLanguageMutation.isPending}
+                selectedLanguage={selectedLanguage}
+                onLanguageChange={setSelectedLanguage}
+                onConfirm={onConfirmLanguage}
+            />
+        );
+    }
 
     return (
         <Stack spacing={5} 
@@ -15,10 +59,10 @@ export default function HomePage() {
             }}>
             {/* TODO: these strings should be internationalized */}
             <Typography variant="h3" component="h1">
-                Welcome back {data?.displayName}
+                Welcome back {languageStats?.displayName}
             </Typography>
             <Typography variant="body1" color="text.secondary">
-                You have practised {data?.uniqueLemmas} unique words in the last {data?.daysPractised} {data?.daysPractised === 1 ? "day" : "days"}.
+                You have practised {languageStats?.uniqueLemmas} unique words in the last {languageStats?.daysPractised} {languageStats?.daysPractised === 1 ? "day" : "days"}.
             </Typography>
             <Button
                 variant="contained"
--- frontend/src/pages/PracticePage.tsx
+++ frontend/src/pages/PracticePage.tsx
@@ -1,11 +1,6 @@
-import LanguageSelector from "../features/languagePractice/components/LanguageSelector";
-import Spinner from "../components/Spinner";
 import SubmissionForm from "../features/languagePractice/components/SubmissionForm";
-import { useGetActiveLanguage, useSetActiveLanguage } from "../features/languagePractice/hooks/useActiveLanguage";
-import { useAvailableLanguages } from "../features/languagePractice/hooks/useAvailableLanguages";
 import { useSubmission } from "../features/languagePractice/hooks/useSubmission";
 import type { SubmissionSchema } from "../features/languagePractice/schemas/submissionSchema";
-import { useState } from "react";
 
 import { useTranslation } from "react-i18next";
 import Alert from "@mui/material/Alert";
@@ -13,20 +8,8 @@ import Stack from "@mui/material/Stack";
 
 export default function PracticePage() {
     const { t } = useTranslation(["common"]);
-    const { data, isLoading, error, refetch } = useGetActiveLanguage();
-    const { data: languages, isLoading: isLoadingLanguages } = useAvailableLanguages();
-    const setLanguageMutation = useSetActiveLanguage();
+
     const submissionMutation = useSubmission();
-    const [selectedLanguage, setSelectedLanguage] = useState("");
-
-    const onConfirmLanguage = async () => {
-        if (!selectedLanguage) {
-            return;
-        }
-
-        await setLanguageMutation.mutateAsync({ languageCode: selectedLanguage });
-        await refetch();
-    };
 
     // If submit succeeds, redirect to submission page (which will show state of analysis and link to analysis once done)
     // If it fails, show error and disable submit button until user changes the input
@@ -34,27 +17,6 @@ export default function PracticePage() {
     const onSubmit = async (formData: SubmissionSchema) => {
         const response = await submissionMutation.mutateAsync(formData);
         console.log("Submission response:", response);
-    }
-
-    if (isLoading) {
-        return <Spinner aria-label={t("common:loading")} />;
-    }
-
-    if (error) {
-        return <Alert severity="error">{t("common:error")}: {error.message}</Alert>;
-    }
-
-    if (!data) {
-        return (
-            <LanguageSelector
-                languages={languages}
-                isLoading={isLoadingLanguages}
-                isSubmitting={setLanguageMutation.isPending}
-                selectedLanguage={selectedLanguage}
-                onLanguageChange={setSelectedLanguage}
-                onConfirm={onConfirmLanguage}
-            />
-        );
     }
 
     return (
